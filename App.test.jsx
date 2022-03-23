f80d112ba7c47e71f6a2297684f61ae73d013dde
aessiq <noriod@yandex.ru>
2022-03-23 21:43:19 +0300

    improve tests, add README

--- App.test.jsx
+++ App.test.jsx
@@ -24,10 +24,50 @@ describe('Calculator renders', () => {
 });
 
 describe('Keyboard input works', () => {
-  it('User ipnut equals display input', () => {
+  it('User input equals display input', () => {
     render(<App />);
-    fireEvent.keyUp()
-  })
+    let buttons = [];
+    for (let i = 0; i < 10; i++) {
+      buttons.push(Math.floor(Math.random() * 10));
+    }
+    buttons.forEach(item => {
+      fireEvent.keyUp(document, { key: `${item}` });
+    })
+    expect(screen.getAllByText(buttons.join())).toBeDefined();
+  });
+  it('Keyboard inputs calculates correctly', () => {
+    render(<App />);
+    fireEvent.keyUp(document, { key: '8' });
+    fireEvent.keyUp(document, { key: '3' });
+    fireEvent.keyUp(document, { key: '+' });
+    fireEvent.keyUp(document, { key: '7' });
+    fireEvent.keyUp(document, { key: '=' });
+    expect(screen.getAllByText('90')).toBeDefined();
+  });
 })
+
+describe('Mouse input works', () => {
+  it('User input equals display input', () => {
+    render(<App />);
+    let buttons = [];
+    for (let i = 0; i < 10; i++) {
+      buttons.push(Math.floor(Math.random() * 10));
+    }
+    buttons.forEach(item => {
+      fireEvent.click(screen.getByText(item));
+    });
+    expect(screen.getAllByText(buttons.join())).toBeDefined();
+  });
+  it('Keyboard inputs calculates correctly', () => {
+    render(<App />);
+    fireEvent.click(screen.getByText('9'));
+    fireEvent.click(screen.getByText('1'));
+    fireEvent.click(screen.getByText('-'));
+    fireEvent.click(screen.getByText('2'));
+    fireEvent.click(screen.getByText('1'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getAllByText('70')).toBeDefined();
+  });
+});
 
 
